32378bcff00fd3d27c652a4c765c2416ff643c2e
Sergey Sidoruk <alf.cat.loverr@gmail.com>
2020-06-15 17:21:42 +0300

    fixed size of page

--- src/styled-components/StyledConsole.js
+++ src/styled-components/StyledConsole.js
@@ -3,8 +3,6 @@ import styled from 'styled-components';
 export const ConsoleContainer = styled.div`
     width: 100%;
     height: 100%;
-    max-width: 144rem;
-    max-height: 73rem;
     display: flex;
     flex-direction: column;
 `;
